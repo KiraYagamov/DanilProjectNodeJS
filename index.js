f0c3ff10649996395a01079309c2b8afcfdd9592
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-03-20 21:02:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,23 +52,84 @@
 // console.log("Символов перед нулями: " + countZero)
 
 
-let line = "dgfdgdf123gfdgdfg123dasdasddsdd"
-let numbers = "1234567890"
-let maxLength = 0
-let currentLength = 0
+// let line = "dgfdgdf123gfdgdfg123dasdasddsdd"
+// let numbers = "1234567890"
+// let maxLength = 0
+// let currentLength = 0
 
-for (let i = 0; i < line.length; i++){
-    if (numbers.includes(line[i])) {
-        currentLength += 1
+// for (let i = 0; i < line.length; i++){
+//     if (numbers.includes(line[i])) {
+//         currentLength += 1
+//     }
+//     else {
+//         if (currentLength > maxLength) {
+//             maxLength = currentLength
+//         }
+//         currentLength = 0
+//     }
+// }
+// if (currentLength > maxLength) {
+//     maxLength = currentLength
+// }
+// console.log(maxLength)
+
+
+// let line = "dgfdgdf123gfdgdfg456dasdasddsdd"
+// let numbers = "1234567890"
+// let summa = 0
+
+// for (let i = 0; i < line.length; i++){
+//     if (numbers.includes(line[i])){
+//         summa += Number(line[i])
+//     }
+// }
+
+// console.log(summa)
+
+
+// function sumOfNumbers(line) {
+//     let numbers = "1234567890"
+//     let summa = 0
+//     for (let i = 0; i < line.length; i++){
+//         if (numbers.includes(line[i])){
+//             summa += Number(line[i])
+//         }
+//     }
+//     return summa
+// }
+
+// let summa1 = sumOfNumbers("dgfdgdf123gfdgdfg456dasdasddsdd")
+// let summa2 = sumOfNumbers("fdsfhhgjhgjbvncv623ngh8769vcnbvbc")
+// let summa3 = sumOfNumbers("cvcxn,b,kdfsd1mvdcb9")
+// let summa4 = sumOfNumbers("gkjlkj;vbncvvxcvds2fn876nbvnbhfgh7765")
+// let summa5 = sumOfNumbers("bcvbvbcn,nm134")
+
+// console.log(summa1 + summa2)
+
+
+function toBinary(num) {
+    if (num === 0) {
+        return "0"
     }
-    else {
-        if (currentLength > maxLength) {
-            maxLength = currentLength
-        }
-        currentLength = 0
+    let result = ""
+    while (num > 0){
+        result = String(num % 2) + result
+        num = Math.floor(num / 2)
     }
+    return result
 }
-if (currentLength > maxLength) {
-    maxLength = currentLength
+
+function pow(num, s) {
+    let result = 1
+    for (let i = 0; i < s; i++) {
+        result *= num
+    }
+    return result
 }
-console.log(maxLength)
+
+function sumArray(arr) {
+
+}
+
+let value = sumArray([5, 7, 2, 3, 8, 9]) // 34
+console.log(value)
